213dc23d8098152a007c78c4772745e2943348e3
Cj Pangilinan <cjpangilinan@gmail.com>
2019-12-16 04:00:04 -0500

    fix error on mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const helmet = require('helmet');
 
 const productsRouter = require('./routes/products');
